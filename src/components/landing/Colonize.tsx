8ec77da922cd3d3349568954a6ed73f0f0a652c7
prithvikr66 <pkunofficial66@gmail.com>
2024-04-10 00:59:56 +0530

    coloze

--- src/components/landing/Colonize.tsx
+++ src/components/landing/Colonize.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import colonizeMars from "../../assets/landing/2.png" 
+import ColonizeMarsMainText, { ColonizeMarsSubText } from "./ColonizeMarsText"
 const Colonize = () => {
   return (
-    <div>Colonize</div>
+    <div className=" relative h-screen bg-[#000000]">
+      <div
+        className="absolute  bg-cover bg-center w-[899px] h-full right-0"
+        style={{ backgroundImage: `url(${colonizeMars})` }}
+      ></div>
+      <ColonizeMarsMainText/>
+      <ColonizeMarsSubText/>
+
+
+    </div>
   )
 }
 
