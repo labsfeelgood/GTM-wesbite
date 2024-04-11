6e4a3c2486e2f44582d9bcadd1b9e69e3f7198b4
prithvikr66 <pkunofficial66@gmail.com>
2024-04-11 18:36:57 +0530

    fixes

--- src/components/landing/Button.tsx
+++ src/components/landing/Button.tsx
@@ -6,7 +6,7 @@ const Button = () => {
         <img
           src={buttonOutline}
           className=" w-[221px] h-[71px]"
-          alt="but gtm"
+          alt=""
         ></img>
         <div className=" absolute inset-0 flex items-center justify-center">
           <p className=" text-white uppercase font-d-din-bold text-[32px] ">
