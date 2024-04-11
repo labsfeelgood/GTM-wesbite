1f6da8075c1eabd94e15392f4aa1605226793a23
prithvikr66 <pkunofficial66@gmail.com>
2024-04-11 18:31:39 +0530

    modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {resolve} from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     alias: {
       "~@fontsource/ibm-plex-mono": "@fontsource/ibm-plex-mono",
       "~@fontsource/inter": "@fontsource/inter",
+      jsbi: resolve(__dirname, '.', 'node_modules', 'jsbi', 'dist', 'jsbi-cjs.js'),
     },
     
   },
