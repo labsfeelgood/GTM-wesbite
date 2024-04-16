d8612621ef38cb0387474cb4d201d2a3b9e61917
prithvikr66 <pkunofficial66@gmail.com>
2024-04-16 16:37:52 +0530

    tax & trasury desktop page

--- src/components/landing/TaxAndTreasury.tsx
+++ src/components/landing/TaxAndTreasury.tsx
@@ -1,67 +1,160 @@
-import peace from "../../assets/landing/peace.png";
-import line1 from "../../assets/landing/line1.png";
 import peaceIcon from "../../assets/landing/Group 804.svg";
 export const TaxAndTreasury = () => {
   return (
-    <div className=" xl:hidden">
-      <h3 className=" font-d-din-bold text-white text-[28px] uppercase sm:text-[32px] md:text-[36px] lg:text-[40px] xl:text-[50px] lg:ml-[50px] xl:ml-[100px] 2xl:ml-[150px] text-left lg:text-left ml-5 sm:ml-10 ">
-        TAX & TREASURY
-      </h3>
-      <div className=" ml-5 sm:ml-11">
-        <p className=" font-d-din-italic text-white text-[16px] sm:text-[20px]">Embrace the Power of 5%</p>
-        <div>
-          <div className=" relative mx-auto">
-            <div className=" bg-[#C2DAF2] h-[2px] w-[130px] "></div>
-            <div className=" relative top-[-15px] bg-[#C2DAF2] h-[15px] w-[2px] rotate-[-30deg] right-[4px]"></div>
+    <>
+      <div className=" xl:hidden">
+        <h3 className=" font-d-din-bold text-white text-[28px] uppercase sm:text-[32px] md:text-[36px] lg:text-[40px] xl:text-[50px] lg:ml-[50px] xl:ml-[100px] 2xl:ml-[150px] text-left lg:text-left ml-5 sm:ml-10 ">
+          TAX & TREASURY
+        </h3>
+        <div className=" ml-5 sm:ml-11">
+          <p className=" font-d-din-italic text-white text-[16px] sm:text-[20px]">
+            Embrace the Power of 5%
+          </p>
+          <div>
+            <div className=" relative mx-auto">
+              <div className=" bg-[#C2DAF2] h-[2px] w-[130px] "></div>
+              <div className=" relative top-[-15px] bg-[#C2DAF2] h-[15px] w-[2px] rotate-[-30deg] right-[4px]"></div>
+            </div>
+          </div>
+        </div>
+        <div className=" display flex flex-col items-center">
+          <div className="  relative">
+            <img src={peaceIcon} className=" md:h-[400px] md:w-[400px] " />
+            <p className=" font-d-din-bold text-white text-[16px] md:text-[24px] absolute inset-0 top-[90px] left-[60px]  md:top-[170px] md:left-[130px]">
+              2%
+            </p>
+            <p className=" font-d-din-bold text-white text-[16px] md:text-[24px] absolute inset-0 top-[90px] left-[135px] md:top-[170px] md:left-[230px]">
+              2%
+            </p>
+            <p className=" font-d-din-bold text-white text-[16px] md:text-[24px] absolute inset-0 top-[150px] left-[100px] md:top-[270px] md:left-[190px]">
+              1%
+            </p>
+          </div>
+        </div>
+        <div className=" flex  flex-col gap-10 p-6 sm:p-10">
+          <div className=" flex flex-col  ">
+            <div className=" ">
+              <div className=" relative top-2">
+                <div className=" bg-[#ffffff] h-[2px] w-[230px] "></div>
+                <div className=" relative   top-[-1px] bg-[#ffffff] h-[15px] w-[2px] rotate-[30deg] left-[-4px]"></div>
+              </div>
+              <p className=" font-d-din-bold text-white  text-[16px] sm:text-[20px]">
+                Technological Advancements - 2%
+              </p>
+            </div>
+            <p className=" font-d-din-regular text-white  text-[12px] sm:text-[16px]">
+              2% is dedicated to Technological Advancements, where innovation
+              knows no bounds
+            </p>
+          </div>
+          <div className=" flex flex-col  ">
+            <div className=" relative top-2 ">
+              <div className=" bg-[#ffffff] h-[2px] w-[230px] "></div>
+              <div className=" relative   top-[-1px] bg-[#ffffff] h-[15px] w-[2px] rotate-[30deg] left-[-4px]"></div>
+            </div>
+            <p className=" font-d-din-bold text-white  text-[16px] sm:text-[20px]">
+              Moon Fund - 2%
+            </p>
+            <p className=" font-d-din-regular text-white  text-[12px] sm:text-[16px]">
+              Within every buy and sell, 2% is allocated to Moon Fund,
+              strategically dedicated to fostering partnerships and elevating
+              our cosmic presence through marketing endeavors
+            </p>
+          </div>
+          <div className=" flex flex-col  ">
+            <div className=" relative  top-2">
+              <div className=" bg-[#ffffff] h-[2px] w-[230px] "></div>
+              <div className=" relative   top-[-1px] bg-[#ffffff] h-[15px] w-[2px] rotate-[30deg] left-[-4px]"></div>
+            </div>
+            <p className=" font-d-din-bold text-white  text-[16px] sm:text-[20px]">
+              Liquidity Pool - 1%
+            </p>
+            <p className=" font-d-din-regular text-white  text-[12px] sm:text-[16px]">
+              Stability is key to a successful space journey. By dedicating 1%
+              of each transaction to liquidity, we ensure that Mission Mars
+              remains on a secure trajectory
+            </p>
           </div>
         </div>
       </div>
-      <div className=" display flex flex-col items-center">
-        <img src={peaceIcon} className=" md:h-[400px] md:w-[400px]"/>
+      {/* --------------------------------- */}
+      <div className=" hidden xl:block h-screen">
+        <div className=" flex justify-between ">
+          <h3 className=" font-d-din-bold text-white text-[28px] uppercase sm:text-[32px] md:text-[36px] lg:text-[40px] xl:text-[50px] lg:ml-[50px] xl:ml-[100px] 2xl:ml-[150px] text-left lg:text-left ml-5 sm:ml-10 ">
+            TAX & TREASURY
+          </h3>
+          <div>
+            <div className=" relative mx-auto">
+              <div className=" bg-[#C2DAF2] h-[2px] w-[180px] "></div>
+              <div className=" relative top-[-15px] bg-[#C2DAF2] h-[15px] w-[2px] rotate-[deg] right-[4px]"></div>
+            </div>
+            <p className=" font-d-din-italic text-[28px] text-white">
+              Embrace the Power of 5%
+            </p>
+          </div>
+        </div>
+        <div className=" flex flex-col items-center">
+          <div className=" relative">
+            <div className="  relative">
+              <img src={peaceIcon} className=" md:h-[400px] md:w-[400px] " />
+              <p className=" font-d-din-bold text-white text-[16px] md:text-[24px] absolute inset-0 top-[90px] left-[60px]  md:top-[170px] md:left-[130px]">
+                2%
+              </p>
+              <p className=" font-d-din-bold text-white text-[16px] md:text-[24px] absolute inset-0 top-[90px] left-[135px] md:top-[170px] md:left-[230px]">
+                2%
+              </p>
+              <p className=" font-d-din-bold text-white text-[16px] md:text-[24px] absolute inset-0 top-[150px] left-[100px] md:top-[270px] md:left-[190px]">
+                1%
+              </p>
+            </div>
+            <div className=" absolute inset-0 top-[70px] left-[-340px]">
+              <div className=" ">
+                <h3 className=" font-d-din-bold text-white text-[28px]">
+                  Technological Advancements
+                </h3>
+                <div className=" relative  top-2">
+                  <div className=" bg-[#ffffff] h-[2px] w-[380px] "></div>
+                  <div className=" relative   top-[-2px] bg-[#ffffff] h-[20px] w-[2px] rotate-[-30deg] left-[383px]"></div>
+                </div>
+              </div>
+              <p className=" font-d-din-regular text-[20px] text-white w-[350px]">
+                2% is dedicated to Technological Advancements, where innovation
+                knows no bounds
+              </p>
+            </div>
+            {/* --------- */}
+            <div className=" absolute inset-0 top-[80px] left-[370px] ">
+              <div className=" w-[400px] ">
+                <h3 className=" font-d-din-bold text-white text-[28px] text-right">
+                  Moon Fund
+                </h3>
+                <div className=" relative  top-2">
+                  <div className=" bg-[#ffffff] h-[2px] w-[380px] "></div>
+                  <div className=" relative   top-[-5px] bg-[#ffffff] h-[20px] w-[2px] rotate-[60deg] left-[-9px]"></div>
+                </div>
+              </div>
+              <p className=" font-d-din-regular text-[20px] text-white w-[400px] text-right">
+                Within every buy and sell, 2% is allocated to Moon Fund,
+                strategically dedicated to fostering partnerships and elevating
+                our cosmic presence through marketing endeavors
+              </p>
+            </div>
+            {/* ----------- */}
+            <div className=" absolute inset-0 top-[350px]">
+              <div className=" ">
+                <div className=" bg-white h-[40px] w-[2px] mx-auto"/>
+                <h3 className=" font-d-din-bold text-white text-[28px] text-center">
+                  Liquidity Pool
+                </h3>
+              </div>
+              <p className=" font-d-din-regular text-[20px] text-white w-[350px] text-center">
+                2% is dedicated to Technological Advancements, where innovation
+                knows no bounds
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className=" flex  flex-col gap-10 p-6 sm:p-10">
-        <div className=" flex flex-col  ">
-          <div className=" relative ">
-            <div className=" bg-[#ffffff] h-[2px] w-[230px] "></div>
-            <div className=" relative   top-[-1px] bg-[#ffffff] h-[15px] w-[2px] rotate-[30deg] left-[-4px]"></div>
-          </div>
-          <p className=" font-d-din-bold text-white  text-[16px] sm:text-[20px]">
-            Technological Advancements - 2%
-          </p>
-          <p className=" font-d-din-regular text-white  text-[12px] sm:text-[16px]">
-            2% is dedicated to Technological Advancements, where innovation
-            knows no bounds
-          </p>
-        </div>
-        <div className=" flex flex-col  ">
-          <div className=" relative ">
-            <div className=" bg-[#ffffff] h-[2px] w-[230px] "></div>
-            <div className=" relative   top-[-1px] bg-[#ffffff] h-[15px] w-[2px] rotate-[30deg] left-[-4px]"></div>
-          </div>
-          <p className=" font-d-din-bold text-white  text-[16px] sm:text-[20px]">
-            Moon Fund - 2%
-          </p>
-          <p className=" font-d-din-regular text-white  text-[12px] sm:text-[16px]">
-            Within every buy and sell, 2% is allocated to Moon Fund,
-            strategically dedicated to fostering partnerships and elevating our
-            cosmic presence through marketing endeavors
-          </p>
-        </div>
-        <div className=" flex flex-col  ">
-          <div className=" relative ">
-            <div className=" bg-[#ffffff] h-[2px] w-[230px] "></div>
-            <div className=" relative   top-[-1px] bg-[#ffffff] h-[15px] w-[2px] rotate-[30deg] left-[-4px]"></div>
-          </div>
-          <p className=" font-d-din-bold text-white  text-[16px] sm:text-[20px]">
-            Liquidity Pool - 1%
-          </p>
-          <p className=" font-d-din-regular text-white  text-[12px] sm:text-[16px]">
-            Stability is key to a successful space journey. By dedicating 1% of
-            each transaction to liquidity, we ensure that Mission Mars remains
-            on a secure trajectory
-          </p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
